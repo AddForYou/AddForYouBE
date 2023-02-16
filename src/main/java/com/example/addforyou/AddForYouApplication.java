cc9bc15123e4be6307d8f04335f43b880d71a4de
choidongkuen <danaver12@daum.net>
2023-02-17 03:12:27 +0900

    initial commit

---
+++ src/main/java/com/example/addforyou/AddForYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.addforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddForYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddForYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/addforyou/AddForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
