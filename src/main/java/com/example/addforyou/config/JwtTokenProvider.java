f3ac25da8d16838c111f70054adc8c4e9e87532d
choidongkuen <danaver12@daum.net>
2023-03-08 21:32:58 +0900

    [#8] feat : JwtTokenProvider createToken 구현
    jwt access-token 발급 후 1시간 만료기간 3600000 ms -> 1시간
    HS256 해시 알고리즘으로 서명

---
+++ src/main/java/com/example/addforyou/config/JwtTokenProvider.java
@@ -0,0 +1,31 @@
+package com.example.addforyou.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+
+public class JwtTokenProvider {
+
+    @Value("${jwt.access-token.expire-length}")
+    private long accessTokenValidityInMilliseconds;
+
+    @Value("${jwt.refresh-token.expire-length}")
+    private long refreshTokenValidityInMilliseconds;
+
+    @Value("${jwt.token.secret-key}")
+    private String secretKey;
+
+    private String createToken(String payload, long expiredLength) {
+        Claims claims = Jwts.claims().setSubject(payload);
+        Date now = new Date();
+        return Jwts.builder()
+                   .setClaims(claims)
+                   .setIssuedAt(now)
+                   .setExpiration(new Date(now.getTime() + expiredLength))
+                   .signWith(SignatureAlgorithm.HS256, secretKey)
+                   .compact();
+    }
+}
