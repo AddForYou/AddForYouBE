16cc0b7d1471f8c566fb25649d1ed3c9191c7667
choidongkuen <danaver12@daum.net>
2023-03-09 12:20:02 +0900

    [#8] feat : JwtTokenProvider 에 createAccessToken , createRefreshToken 메소드 추가

--- src/main/java/com/example/addforyou/config/JwtTokenProvider.java
+++ src/main/java/com/example/addforyou/config/JwtTokenProvider.java
@@ -1,11 +1,13 @@
 package com.example.addforyou.config;
 
+import com.example.addforyou.dto.Token;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class JwtTokenProvider {
 
@@ -18,7 +20,7 @@ public class JwtTokenProvider {
     @Value("${jwt.token.secret-key}")
     private String secretKey;
 
-    private String createToken(String payload, long expiredLength) {
+    private String createToken(String payload, long expiredLength) { // 순수 토큰 생성
         Claims claims = Jwts.claims().setSubject(payload);
         Date now = new Date();
         return Jwts.builder()
@@ -27,5 +29,24 @@ public class JwtTokenProvider {
                    .setExpiration(new Date(now.getTime() + expiredLength))
                    .signWith(SignatureAlgorithm.HS256, secretKey)
                    .compact();
+    }
+
+    private Token createAccessToken(String payload) { // Access Token 생성
+        String token = createToken(payload, accessTokenValidityInMilliseconds);
+
+        return Token.builder()
+                .token(token)
+                .expiredTime(accessTokenValidityInMilliseconds)
+                .build();
+    }
+
+    private Token createRefreshToken(String payload) { // Refresh Token 생성
+        String token = createToken(UUID.randomUUID().toString(), accessTokenValidityInMilliseconds);
+
+        return Token.builder()
+                .token(token)
+                .expiredTime(accessTokenValidityInMilliseconds)
+                .build();
+
     }
 }
