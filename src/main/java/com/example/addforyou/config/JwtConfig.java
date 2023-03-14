0eaa410963a6f2f9649b550c7812c7dc99c8e52e
choidongkuen <danaver12@daum.net>
2023-03-15 02:53:42 +0900

    [#10] feat : JwtConfig 클래스 구현

--- src/main/java/com/example/addforyou/config/JwtConfig.java
+++ src/main/java/com/example/addforyou/config/JwtConfig.java
@@ -1,4 +1,13 @@
 package com.example.addforyou.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class JwtConfig {
+
+    @Bean
+    public JwtTokenProvider jwtAuthenticationProvider() {
+        return new JwtTokenProvider();
+    }
 }
